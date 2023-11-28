5fcad94e7b50d2706c6a89a6b7019f9d103bef8d
walk8243 <walk8243@gmail.com>
2023-11-29 00:47:02 +0900

    modeの受け渡しの設定

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useContext } from 'react'
+import { ModeContext } from '../context/mode'
+import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  const mode = useContext(ModeContext)
+  return (
+    <ModeContext.Provider value={mode}>
+      <Component {...pageProps} />
+    </ModeContext.Provider>
+  )
 }
 
 export default MyApp
